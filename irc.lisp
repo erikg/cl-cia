df37e374da2d6d3393f8ada56a0e7cbbd5f69cfc
Erik Greenwald <erik@elfga.com>
2012-11-06 12:30:07 -0500

    colorize messages

--- irc.lisp
+++ irc.lisp
@@ -4,8 +4,21 @@
 (defvar *bot-thread* '())
 (defvar *connection* '())
 
+(defun filestr (files)
+  (if (> (length files) 1)
+      (format nil "(~{~a~^ ~})" files)
+      (car files)))
+
+(defun ascii-ize (str col)
+  (format nil "~C~2,'0d~a~C" #\etx col str #\etx))
+
+(defun format-commit (message)
+  (format nil "~a ~a: ~a"
+	  (ascii-ize (format nil "~a * ~a" (user message) (revision message)) 3)
+	  (filestr (files message))
+	  (message message)))
 (defun report-commit (message)
-  (cl-irc:privmsg *connection* "#brlcad" (format nil "~a:~a ~a" (user message) (revision message) (message message))))
+  (cl-irc:privmsg *connection* "#brlcad" (format-commit message)))
 
 (defun bot (&key (nick "brlbot") (server "irc.freenode.net") (channel "#brlcad"))
   (setf *connection* (cl-irc:connect :username nick :realname "BRL Bot" :server server :nickname nick))
