3fb73cef6ff6017cad2d9b5a6da9a6b53eefedf7
Erik Greenwald <erik@elfga.com>
2012-11-06 08:29:06 -0500

    add maildir pump and start/stop wrappers

--- cl-cia.lisp
+++ cl-cia.lisp
@@ -25,3 +25,30 @@
 (defvar *messages* '())
 (defun message-seen (message &optional (messages *messages*))
   (find message messages :test #'equals))
+
+(defun pump ()
+  (process-mail-dir :hook
+		    (list
+		     #'report-commit)))
+
+(defvar *pump* '())
+(defvar *pump-running* '())
+(defun start-pump ()
+  (unless *pump*
+    (setf *pumper* (bordeaux-threads:make-thread
+		    (lambda ()
+		      (loop while *pump-running* do (progn
+						      (pump) (sleep 5))))))))
+(defun stop-pump ()
+  (when (and *pump* *pump-running*)
+    (setf *pump-running* '())
+    (bordeaux-threads:join-thread *pump*)
+    (setf *pump* '())))
+
+(defun start ()
+  (bot)
+  (sleep 5)
+  (start-pump))
+(defun stop ()
+  (stop-bot)
+  (stop-pump))
