94fd004638ecc2214e7e7ca97005ae9f0290a123
Erik Greenwald <erik@elfga.com>
2012-11-06 07:46:06 -0500

    process a maildir, with locking and callbacks on new msg

--- cl-cia.lisp
+++ cl-cia.lisp
@@ -2,11 +2,14 @@
 
 (in-package #:cl-cia)
 
-(defparameter +dbdir+ (merge-pathnames "db/cia/" (user-homedir-pathname)))
+(defparameter +db-dir+ (merge-pathnames "db/cia/" (user-homedir-pathname)))
 
 ; mail form assumes the procmail matching rule puts it in $HOME/db/cia/mail/
 ; with the trailing slash inferring mailbox dir instead of mbox file.
-(defparameter +dbmaildir+ (merge-pathnames "mail/" +dbdir+))
+(defparameter +db-mail-dir+ (merge-pathnames "mail/" +db-dir+))
+(defparameter +db-unprocessed-mail-dir+ (merge-pathnames "unprocessed/" +db-mail-dir+))
+
+(defvar *biglock* (bordeaux-threads:make-lock "cl-cia"))
 
 (defclass commit ()
   ((timestamp :accessor timestamp :initarg :timestamp :initform (local-time:now))
@@ -18,6 +21,10 @@
   (format stream "#<Commit: ~a@~a: ~a (at ~a)>" (user c) (revision c) (message c) (timestamp c)))
 (defmethod equals ((c1 commit) (c2 commit))
   (and (string-equal (user c1) (user c2)) (= (revision c1) (revision c2))))
+
+(defvar *messages* '())
+(defun message-seen (message &optional (messages *messages*))
+  (find message messages :test #'equals))
 
 (defun read-file-to-list (file)
   (with-open-file (stream file)
@@ -48,7 +55,7 @@
   (let* ((f (read-file-to-list mailfile))
 ;	 (subj (snarf "^Subject:" f))
 	 (date (datify (snarf "^Date:" f)))
-	 (revision (snarf "^Revision:" f))
+	 (revision (parse-integer (snarf "^Revision:" f) :junk-allowed t))
 	 (author (snarf "^Author:" f))
 	 (body '()))
     (let ((start (+ (position-if (lambda (x) (cl-ppcre:scan "^Log Message:$" x)) f) 2))
@@ -56,3 +63,12 @@
       (when (and start end)
 	(setf body (format nil "~{~a ~}" (subseq f start end)))))
     (make-instance 'commit :revision revision :timestamp date :user author :message body)))
+
+(defun process-mail-dir (&key (maildir +db-unprocessed-mail-dir+) (hooks '()))
+  "Parse all messages in a mail dir, adding parsed commit messages to the list and applying the hooks"
+  (bordeaux-threads:with-lock-held (*biglock*)
+    (dolist (file (cl-fad:list-directory maildir))
+      (alexandria:when-let ((message (load-commit-from-mail-message file)))
+	(unless (message-seen message)
+	  (push message *messages*)
+	  (dolist (h hooks) (funcall h message)))))))
