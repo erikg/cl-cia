4ecf524925546d6b3a98f5a1108ba489aade4ce2
Erik Greenwald <erik@elfga.com>
2012-11-22 11:08:09 -0500

    rewrite mail parser

--- maildir.lisp
+++ maildir.lisp
@@ -7,28 +7,6 @@
     (loop for line = (read-line stream nil)
        while line collect line)))
 
-(defun snarf (regex lines)
-  "Return contents of a : type line matching regex"
-  (let ((start '()) (end '()) (r1 '()) (r2 '()) (l '()))
-    (find-if (lambda (x)
-	       (setf l x)
-	       (multiple-value-setq (start end r1 r2)
-		 (cl-ppcre:scan (concatenate 'string regex "[ \t]*(.*)[ \t]*$") x))
-	       start)
-	     lines)
-    (when (and r1 r2)
-      (subseq l (aref r1 0) (aref r2 0)))))
-
-(defun snarf-all (regex lines)
-  (remove nil
-	  (mapcar
-	   (lambda (x)
-	     (multiple-value-bind (start end r1 r2)
-		 (cl-ppcre:scan (concatenate 'string regex "[ \t\]*(.*)[ \t]*$") x)
-	       (when (and start end r1 r2)
-		 (subseq x (aref r1 0) (aref r2 0)))))
-	   lines)))
-
 (defun datify (datestr)
   "Attempt to convert a string to a local-time:timestamp object, using local-time:now if unable"
   ; and it always seems unable.
@@ -38,23 +16,60 @@
 	(local-time::invalid-timestring () (local-time:now)))
       (local-time:now)))
 
+(defun isarg (line)
+  (when line (cl-ppcre:scan "^[A-Z][^:]*:" line)))
+
+(defun trimmulti (lines offset)
+  ""
+  (if (<= (length (car lines)) offset)
+      (cdr lines)
+      (cons (string-trim " " (subseq (car lines) offset)) (cdr lines))))
+
+(defun fieldinate (lines)
+  "Given a set of lines following the email field format, build an association list of fields"
+  (when lines
+    (multiple-value-bind (start end) (isarg (car lines))
+      (unless start (return-from fieldinate '()))
+      (let ((nextargline (position-if #'isarg (cdr lines))))
+	(let ((argname (subseq (car lines) start (- end 1))))
+	  (cons
+	   (cons argname
+		 (if (or (not nextargline) (> nextargline 1))
+		     (trimmulti (subseq lines 0 (when nextargline (+ nextargline 1))) end)
+		     (list (string-trim " " (subseq (car lines) end)))))
+	   (fieldinate (if nextargline
+			   (nthcdr (+ nextargline 1) lines)
+			   (cdr lines)))))))))
+
+(defun flob (&optional (mailfile #P"/home/erik/db/cia/mail2/new/1353538242.48694_2.crit.brlcad.org"))
+  (alexandria:when-let ((lines (read-file-to-list mailfile)))
+    (alexandria:when-let ((p (position-if (lambda (x) (string= "" x)) lines)))
+      (alexandria:when-let ((header (fieldinate (subseq lines 0 p)))
+			    (body (fieldinate (subseq lines (+ p 1)))))
+	(values header body)))))
+
+(defun mail-element (name set)
+  (let ((l (remove "" (mapcar (lambda (x) (string-trim " " x)) (cdr (assoc name set :test #'string=))) :test #'string=)))
+    (if (cdr l) l (car l))))
 (defun load-commit-from-mail-message (mailfile)
-  (let* ((f (read-file-to-list mailfile))
-;	 (subj (snarf "^Subject:" f))
-	 (list-id (snarf "^List-Id:" f))
-	 (date (datify (snarf "^Date:" f)))
-	 (revision (snarf "^Revision:" f))
-	 (author (snarf "^Author:" f))
-	 (files (snarf-all "^Modified:" f))
-	 (body '()))
-    (when (and revision author)
-      (let ((start (+ (position-if (lambda (x) (cl-ppcre:scan "^Log Message:$" x)) f) 2))
-	    (end (position-if (lambda (x) (cl-ppcre:scan "^Modified Paths:$" x)) f)))
-	(when (and start end)
-	  (setf body (format nil "~{~a ~}" (subseq f start end)))))
+  (multiple-value-bind (header body)
+      (flob mailfile)
+    (alexandria:when-let ((list-id (mail-element "List-Id" header))
+			  ;; favor the body Date node, but fall back to header if needed
+			  (date (datify (or (mail-element "Date" body) (mail-element "Date" header))))
+			  (revision (or (mail-element "Revision" body) (mail-element "New Revision" body)))
+			  (author (mail-element "Author" body))
+			  ;; this probably needs more work
+			  (files (or (mail-element "Modified Paths" body) (mail-element "Modified" body)))
+			  ;; the actual commit log message
+			  (log (or (mail-element "Log" body) (mail-element "Log Message" body))))
+      ;; SVN::Notify likes to shove "-----" lines in, so try to eat those
+      (when (and (listp log) (cl-ppcre:scan "^-*$" (car log)) (pop log)))
+      (when (and (listp files) (cl-ppcre:scan "^-*$" (car files))) (pop files))
+      (unless (eq (type-of files) 'list) (setf files (list files)))
       (values
-       (make-instance 'commit :files files :revision revision :date date :user author :message body)
-       (subseq list-id 0 (position #\Space list-id))))))
+       (make-instance 'commit :files files :revision revision :date date :user author :message log)
+       list-id))))
 
 (defun process-mail-dir (&key (maildir +db-unprocessed-mail-dir+) (hooks '()))
   "Parse all messages in a mail dir, adding parsed commit messages to the list and applying the hooks"
